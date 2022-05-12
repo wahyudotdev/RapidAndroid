6ca3ef641f5080c13d5ed9d32ec147b9416b9040
wahyudotdev <wahyu.smallker@gmail.com>
2022-05-12 07:29:00 +0700

    feat: add base fragment

---
+++ core/src/main/java/com/rapidcore/core/base/fragment/BaseFragment.kt
@@ -0,0 +1,25 @@
+package com.rapidcore.core.base.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VB : ViewDataBinding>(@LayoutRes private val layoutRes: Int): Fragment() {
+
+    protected lateinit var binding: VB
+
+    open var title: String = ""
+    open var hasLoadedOnce = false
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
