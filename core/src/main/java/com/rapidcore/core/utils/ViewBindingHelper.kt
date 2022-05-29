489dbe563efa275f90a2cc192a12e7367c3fae47
wahyudotdev <wahyu.smallker@gmail.com>
2022-05-29 19:18:45 +0700

    feat: add empty field error

--- core/src/main/java/com/rapidcore/core/utils/ViewBindingHelper.kt
+++ core/src/main/java/com/rapidcore/core/utils/ViewBindingHelper.kt
@@ -1,14 +1,18 @@
 package com.rapidcore.core.utils
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.core.widget.doOnTextChanged
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.size.Scale
 import coil.transform.CircleCropTransformation
 import coil.transform.RoundedCornersTransformation
 import coil.transform.Transformation
+import com.google.android.material.textfield.TextInputLayout
 
 object ViewBindingHelper {
 
@@ -43,6 +47,30 @@ object ViewBindingHelper {
                 }
                 transformations(transform)
             }
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("emptyFieldError")
+    fun emptyFieldError(et: TextInputLayout, emptyFieldError: String?) {
+        emptyFieldError?.let {
+            et.editText?.doOnTextChanged { text, _, _, _ ->
+                setEtError(et, text.isNullOrEmpty(), emptyFieldError)
+            }
+            et.editText?.setOnFocusChangeListener { _, _ ->
+                setEtError(et, et.editText?.text?.isEmpty() == true, emptyFieldError)
+            }
+        }
+    }
+    private fun setEtError(et: TextInputLayout, error: Boolean, message: String?){
+        if (error){
+            et.isErrorEnabled = true
+            et.errorIconDrawable = null
+            et.error = message
+            et.boxStrokeErrorColor = ColorStateList.valueOf(Color.RED)
+        } else {
+            et.isErrorEnabled = false
+            et.error = null
         }
     }
 }
