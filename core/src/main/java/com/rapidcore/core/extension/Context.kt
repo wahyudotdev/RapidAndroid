c7a868d64019bad9bf6e56348f2b799bcf4ecbad
wahyudotdev <wahyudotdev@gmail.com>
2022-11-24 06:50:35 +0700

    feat: adding toast extension on Context.kt

--- core/src/main/java/com/rapidcore/core/extension/Context.kt
+++ core/src/main/java/com/rapidcore/core/extension/Context.kt
@@ -1,6 +1,7 @@
 package com.rapidcore.core.extension
 
 import android.content.Context
+import android.widget.Toast
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
@@ -8,3 +9,8 @@ import com.rapidcore.core.utils.Config
 
 @Suppress("unused")
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(Config.PreferenceKey.DATA_STORE_APP_NAME)
+
+@Suppress("unused")
+fun Context.tos(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
